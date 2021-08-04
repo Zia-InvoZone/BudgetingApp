95487499f9bb8b8b3fafb3c67361bc2de790eac4
waqar-invozone <muhammad.waqar@invozone.com>
2021-08-04 18:01:52 +0500

    expense-attachment

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 require('dotenv').config();
+const Sequelize = require('sequelize');
 const port = process.env.PORT;
 const express = require('express');
 const app = express();
-
+const User = require('./models').user;
 app.get('/', (req, res) => {
-  res.send('2nd test!');
+  return res.send('welcome');
 });
+app.get('/add-user', (req, res) => {
+  // sample user testing for insertion
+  return User.create({
+    name: 'test ',
+    email: 'test@example.com',
+    password: '123',
+  })
+    .then((user) => res.status(201).send(user))
+    .catch((error) => res.status(400).send(error));
+});
+app.get('*', (req, res) =>
+  res.status(400).send({
+    message: 'Your Lost',
+  })
+);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
--- models/ExpenseAttachments.js
+++ models/ExpenseAttachments.js
@@ -1,29 +1,27 @@
+'use strict';
 const { Model } = require('sequelize');
-
 module.exports = (sequelize, DataTypes) => {
-  class Expense extends Model {
+  class ExpenseAttachments extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    // static associate(models) {
-    // define association here
-    // }
+    static associate(models) {
+      // define association here
+    }
   }
-  Expense.init(
+  ExpenseAttachments.init(
     {
-      type: DataTypes.STRING,
-      amount: DataTypes.STRING,
-      description: DataTypes.STRING,
-      created_by: DataTypes.STRING,
+      file: DataTypes.STRING,
+      expense_id: DataTypes.INTEGER,
     },
     {
       sequelize,
-      modelName: 'expense',
+      modelName: 'expense_attachment',
       createdAt: 'created_at',
       updatedAt: 'updated_at',
     }
   );
-  return Expense;
+  return ExpenseAttachments;
 };
